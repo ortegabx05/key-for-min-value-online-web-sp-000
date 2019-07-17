3ce01d157081b0cee6aa0e18d87571efd742783d
jessi ortega <ortegabx05@gmail.com>
2019-07-17 18:59:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,6 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   else
-      name_hash.each do |key, value|
-        if value < min_value
-          min_value == value
-          min_key == key
-        end
-      end
       min_key
   end 
 end
