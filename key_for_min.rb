58279b9ad282ae43d64a72eae2cc864a1dd1ae9e
jessi ortega <ortegabx05@gmail.com>
2019-07-17 18:57:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash = {}
+    return nil
+  else
+      name_hash.each do |key, value|
+        if value < min_value
+          min_value = value
+          min_key = key
+        end
+      end
+      min_key
+  end 
+enddef key_for_min_value(name_hash)
 
 end
